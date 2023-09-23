e1f581cb6bf38da81a9d35ff332fe80440938484
Faizan <faizan1041@users.noreply.github.com>
2023-09-23 11:16:22 +0500

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -6,10 +6,12 @@ SCRIPTPATH=`pwd`
 mkdir -p $SCRIPTPATH/data
 mkdir -p $SCRIPTPATH/data/script/Arabic
 
-wget -O $SCRIPTPATH/data/Latin.unicharset  https://github.com/tesseract-ocr/langdata_lstm/raw/master/Latin.unicharset
-wget -O $SCRIPTPATH/data/Arabic.unicharset  https://github.com/tesseract-ocr/langdata_lstm/raw/master/Arabic.unicharset
+wget -O $SCRIPTPATH/data/Latin.unicharset  https://github.com/tesseract-ocr/langdata_lstm/raw/main/Latin.unicharset
+wget -O $SCRIPTPATH/data/Arabic.unicharset  https://github.com/tesseract-ocr/langdata_lstm/raw/main/Arabic.unicharset
 cp ~/langdata_lstm/Inherited.unicharset $SCRIPTPATH/data/
 
-cp ~/tessdata_fast/ara.traineddata $SCRIPTPATH/data/fast_ara.traineddata
-cp ~/tessdata_fast/script/Arabic.traineddata $SCRIPTPATH/data/fast_Arabic.traineddata
+# cp ~/tessdata_fast/ara.traineddata $SCRIPTPATH/data/fast_ara.traineddata
+# cp ~/tessdata_fast/script/Arabic.traineddata $SCRIPTPATH/data/fast_Arabic.traineddata
 
+wget -O $SCRIPTPATH/data/fast_ara.traineddata https://github.com/tesseract-ocr/tessdata_fast/raw/main/ara.traineddata
+wget -O $SCRIPTPATH/data/fast_Arabic.traineddata https://github.com/tesseract-ocr/tessdata_fast/raw/main/script/Arabic.traineddata
